5b3594ec350c79e50dea5d45c76e251ab3dc304d
truongquangduy2002 <85172633+truongquangduy2002@users.noreply.github.com>
2022-09-22 00:13:52 +0700

    Laravel_Udemy_2022

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('admin.index');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
+
+Route::controller(AdminController::class)->group(function () {
+    // Route::get('/admin/dashboard', 'index')->name('dashboard');
+    Route::get('/admin/logout', 'destroy')->name('admin.logout');
+});
